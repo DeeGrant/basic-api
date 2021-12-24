8d3ab8085d46076e08d1aff42b89747b5670cef1
Dee Grant <WalterGrant@gmail.com>
2021-12-23 21:19:13 -0600

    display api data in dom

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 document.querySelector('button').addEventListener('click', getRoses)
 
 async function getRoses() {
+    const flower = document.querySelector('input').value
     try {
-        const response = await fetch('http://localhost:8000/api/roses')
+        const response = await fetch('http://localhost:8000/api/flowers/'+ flower)
         const data = await response.json()
-        console.log(data)
+        const result = `${data.number} ${data.what} ${data.number === 1 ? "is" : "are"} ${data.color}!`
+        document.querySelector('p').innerText = result
     } catch (e) {
         console.log(e)
     }
 }
 
-// paragraph = document.querySelector('p')
 
--- server.js
+++ server.js
@@ -23,6 +23,30 @@ app.get('/api/roses', (request, response) => {
     response.json(roses)
 })
 
+let flowers = {
+    'roses': roses,
+    'daisies': {
+        'what': 'Daisies',
+        'color': 'White',
+        'number': 3
+    },
+    'lily': {
+        'what': 'Lily',
+        'color': 'Orange',
+        'number': 1
+    }
+}
+
+app.get('/api/flowers/:flower', (req, res) => {
+    const flower = req.params.flower.toLowerCase()
+    console.log(flower)
+    if (flowers[flower]) {
+        res.json(flowers[flower])
+    } else {
+        res.json(flowers['lily'])
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
 })
